4bb8de0e927cca30dec9a9f2dbb1c0282fd30995
MKalisiak <michal.c.kalisiak@gmail.com>
2019-09-11 14:30:21 +0200

    WIP

--- test.py
+++ test.py
@@ -1,25 +1,35 @@
-#
-# def create_dict(list1, list2) -> dict:
-#     place = {list1[i]: list2[i] for i in range(len(list2))}
-#     return place
-# countries = ["Angola", "Anglia", "Boliwia","Czechy", "Dania", "Polska"]
-# names = ["Alibaba", "Andrzej", "Bożo", "Czarek", "Daniel", "Bambo"]
-# # place = {}
+# #
+# # def create_dict(list1, list2) -> dict:
+# #     place = {list1[i]: list2[i] for i in range(len(list2))}
+# #     return place
+# # countries = ["Angola", "Anglia", "Boliwia","Czechy", "Dania", "Polska"]
+# # names = ["Alibaba", "Andrzej", "Bożo", "Czarek", "Daniel", "Bambo"]
+# # # place = {}
+# # # print(place)
+# # # for i in range(len(names)):
+# # #     place[countries[i]] = names[i]
+# #
+# # # place={names[i]: countries[i] for i in range(len(countries))}
+# #
+# # place = create_dict(list1=names, list2=countries)
+# #
 # # print(place)
-# # for i in range(len(names)):
-# #     place[countries[i]] = names[i]
 #
-# # place={names[i]: countries[i] for i in range(len(countries))}
+# def power(base: int, pow: int) -> int:
+#     # return base to pow power
+#     # x, y = base, pow
+#     # base = 2
+#     # pow = 4
+#     result = base**pow
+#     return result
 #
-# place = create_dict(list1=names, list2=countries)
-#
-# print(place)
+# mmm = power(2,4)
+# print(mmm)
 
-def power(base: int, pow: int) -> int:
-    # return base to pow power
-    # x, y = base, pow
-    result = base**pow
-    return result
-
-mmm = power(2,4)
-print(mmm)
+# def list(iterable):
+#     result = []
+#     for item in iterable:
+#         result.append(item)
+#     return result
+for i in []:
+    print("yp")
